a560dee6ba066a52e4ae55bf1c17b33ab59b0e65
AbdulGafar <jimohtobi@gmail.com>
2016-06-15 08:26:07 -0700

    Added Splash Screen and Dialog Class (#1)

--- app/src/main/java/com/et/fivekilled/HomeMenu.java
+++ app/src/main/java/com/et/fivekilled/HomeMenu.java
@@ -2,26 +2,33 @@ package com.et.fivekilled;
 
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import Helpers.FiveKilledDialog;
+
 public class HomeMenu extends AppCompatActivity {
     FloatingActionButton spbtn;
     FloatingActionButton mpbtn;
+    FragmentManager fm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_menu);
+        fm = getFragmentManager();
 
         spbtn = (FloatingActionButton) findViewById(R.id.singlePlayerButton);
-        mpbtn = (FloatingActionButton) findViewById(R.id.multiPlayerButton);
+       // mpbtn = (FloatingActionButton) findViewById(R.id.multiPlayerButton);
         spbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(HomeMenu.this,SinglePlayerActivity.class);
-                startActivity(i);
+                FiveKilledDialog fk = new FiveKilledDialog();
+                fk.show(fm,"");
+//                Intent i = new Intent(HomeMenu.this,SinglePlayerActivity.class);
+//                startActivity(i);
             }
         });
     }
