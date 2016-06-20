6ab3f20fe4500301ab991b41af3a662282364462
jnrshb <jnrshb35@gmail.com>
2016-06-20 07:51:18 +0000

    all activities set to FullScreen.

--- app/src/main/java/com/et/fivekilled/HomeMenu.java
+++ app/src/main/java/com/et/fivekilled/HomeMenu.java
@@ -5,6 +5,8 @@ import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import Helpers.FiveKilledDialog;
 
@@ -22,6 +24,8 @@ public class HomeMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home_menu);
         fm = getFragmentManager();
 
--- app/src/main/java/com/et/fivekilled/SinglePlayerActivity.java
+++ app/src/main/java/com/et/fivekilled/SinglePlayerActivity.java
@@ -14,6 +14,8 @@ import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import android.widget.EditText;
@@ -73,6 +75,8 @@ public class SinglePlayerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_single_player);
         fkDialog = new FiveKilledDialog();
         dialogArgs = new Bundle();
